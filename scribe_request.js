43ac7613d6ef76c79f106e31e4e21b03aebe79e5
Gabriel Cuendet <gabriel.cuendet@gmail.com>
2025-03-24 20:49:23 +0100

    Initial commit

---
+++ scribe_request.js
@@ -0,0 +1,35 @@
+async function uploadAudio() {
+    const fileInput = document.getElementById('audioFile');
+    const file = fileInput.files[0];
+    const resultDiv = document.getElementById('result');
+
+    if (!file) {
+        resultDiv.textContent = 'Please select an audio file.';
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('audio', file);
+    
+    const tokenInfo = fiefAuth.getTokenInfo();
+    const accessToken = tokenInfo.access_token;
+
+    try {
+        const response = await fetch('https://yvuzpc7gj8.execute-api.eu-central-1.amazonaws.com/scribe/api/v1/audiomessage', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            resultDiv.textContent = `API Response: ${JSON.stringify(data)}`;
+        } else {
+            resultDiv.textContent = 'Error uploading file.';
+        }
+    } catch (error) {
+        resultDiv.textContent = `Error: ${error.message}`;
+    }
+}
